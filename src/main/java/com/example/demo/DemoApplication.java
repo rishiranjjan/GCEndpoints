e278bdf56f7eeae7ae16d55263041bbcb2fa91c6
anamika639choudhary <anamika639choudhary@gmail.com>
2026-06-07 21:27:16 +0530

    updated the hello world!

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -25,7 +25,7 @@ public class DemoApplication {
 	}
 	@GetMapping("/hello")
 	public String hello(){
-		return "Hello World";
+		return "Hello World!";
 	}
 	    @Bean
     public Docket api(ServletContext servletContext) { 
